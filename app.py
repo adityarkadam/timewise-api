1752ca3159530f1a7254f7eb413e4cddee08dfd8
adityarkadam <101013329+adityarkadam@users.noreply.github.com>
2024-05-01 19:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -97,4 +97,4 @@ def ingredient_extraction(input_parameters :model_input):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
